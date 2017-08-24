c92664af12a906546848d10949e00767cafdac87
Himanshu Dixit <hudixt@gmail.com>
2017-08-24 13:21:45 +0530

    Initial Commit

---
+++ src/Form/DropboxAuthSettingsForm.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Drupal\social_auth_dropbox\Form;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Path\PathValidatorInterface;
+use Drupal\Core\Routing\RequestContext;
+use Drupal\Core\Routing\RouteProviderInterface;
+use Drupal\social_auth\Form\SocialAuthSettingsForm;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Settings form for Social Auth Dropbox.
+ */
+class DropboxAuthSettingsForm extends SocialAuthSettingsForm {
+
+  /**
+   * The request context.
+   *
+   * @var \Drupal\Core\Routing\RequestContext
+   */
+  protected $requestContext;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration factory.
+   * @param \Drupal\Core\Routing\RouteProviderInterface $route_provider
+   *   Used to check if route exists.
+   * @param \Drupal\Core\Path\PathValidatorInterface $path_validator
+   *   Used to check if path is valid and exists.
+   * @param \Drupal\Core\Routing\RequestContext $request_context
+   *   Holds information about the current request.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, RouteProviderInterface $route_provider, PathValidatorInterface $path_validator, RequestContext $request_context) {
+    parent::__construct($config_factory, $route_provider, $path_validator);
+    $this->requestContext = $request_context;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this class.
+    return new static(
+    // Load the services required to construct this class.
+      $container->get('config.factory'),
+      $container->get('router.route_provider'),
+      $container->get('path.validator'),
+      $container->get('router.request_context')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'social_auth_dropbox_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return array_merge(
+      parent::getEditableConfigNames(),
+      ['social_auth_dropbox.settings']
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('social_auth_dropbox.settings');
+
+    $form['dropbox_settings'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Dropbox Client settings'),
+      '#open' => TRUE,
+      '#description' => $this->t('You need to first create a Dropbox App at <a href="@dropbox-dev">@dropbox-dev</a>', ['@dropbox-dev' => 'https://www.dropbox.com/developers/apps']),
+    ];
+
+    $form['dropbox_settings']['app_key'] = [
+      '#type' => 'textfield',
+      '#required' => TRUE,
+      '#title' => $this->t('App Key'),
+      '#default_value' => $config->get('app_key'),
+      '#description' => $this->t('Copy the App Key here.'),
+    ];
+
+    $form['dropbox_settings']['app_secret'] = [
+      '#type' => 'textfield',
+      '#required' => TRUE,
+      '#title' => $this->t('App Secret'),
+      '#default_value' => $config->get('app_secret'),
+      '#description' => $this->t('Copy the App Secret here.'),
+    ];
+
+    $form['dropbox_settings']['authorized_redirect_url'] = [
+      '#type' => 'textfield',
+      '#disabled' => TRUE,
+      '#title' => $this->t('Authorized redirect URIs'),
+      '#description' => $this->t('Copy this value to <em>Authorized redirect URIs</em> field of your Dropbox App settings.'),
+      '#default_value' => $GLOBALS['base_url'] . '/user/login/dropbox/callback',
+    ];
+
+    $form['dropbox_settings']['authorized_javascript_origin'] = [
+      '#type' => 'textfield',
+      '#disabled' => TRUE,
+      '#title' => $this->t('Authorized Javascript Origin'),
+      '#description' => $this->t('Copy this value to <em>Authorized Javascript Origins</em> field of your Dropbox App settings.'),
+      '#default_value' => $this->requestContext->getHost(),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+    $this->config('social_auth_dropbox.settings')
+      ->set('app_key', $values['app_key'])
+      ->set('app_secret', $values['app_secret'])
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
---
+++ src/Plugin/Network/DropboxAuthInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\social_auth_dropbox\Plugin\Network;
+
+use Drupal\social_api\Plugin\NetworkInterface;
+
+/**
+ * Defines the Dropbox Auth interface.
+ */
+interface DropboxAuthInterface extends NetworkInterface {}
