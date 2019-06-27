4a4485b3d962351fe5bddb8aeaa09029d3877203
Getulio Sánchez <valentin2507@gmail.com>
2019-06-26 23:00:57 -0400

    Issue #3064389: Call checkAuthError to check for authentication errors

--- src/Plugin/Network/DropboxAuth.php
+++ src/Plugin/Network/DropboxAuth.php
@@ -33,7 +33,7 @@ class DropboxAuth extends NetworkBase implements DropboxAuthInterface {
    * @return \Stevenmaguire\OAuth2\Client\Provider\Dropbox|false
    *   The initialized 3rd party library instance.
    *
-   * @throws SocialApiException
+   * @throws \Drupal\social_api\SocialApiException
    *   If the SDK library does not exist.
    */
   protected function initSdk() {
